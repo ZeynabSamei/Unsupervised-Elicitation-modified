dad8aceeacf5672b9462f8ed5573aebf0da2000a
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2025-09-24 23:33:34 +0330

    Update path_utils.py

--- src/tools/path_utils.py
+++ src/tools/path_utils.py
@@ -8,4 +8,5 @@ def get_root_directory():
 
 
 def get_default_results_directory():
-    return get_root_directory() / "results"
+    return "/home/maliza/scratch/results"
+    # return get_root_directory() / "results"
