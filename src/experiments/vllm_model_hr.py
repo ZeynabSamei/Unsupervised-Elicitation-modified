711a2660e63fe535f520b43f09d0ddf569d6ef1f
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2025-10-04 17:13:15 +0330

    Update vllm_model_hr.py

--- src/experiments/vllm_model_hr.py
+++ src/experiments/vllm_model_hr.py
@@ -114,9 +114,9 @@ def run_for_dataset(dataset_name, save_name, args, client):
 
 def main(args):
     categories = [
-        "Appearance", "Continent of origin", "Country", "Disability", "Gender",
+        "Appearance", "Continent", "Country", "Disability", "Gender",
         "Nationality", "Personality", "Politics", "Race_Ethnicity",
-        "Religion", "Sexual orientation", "Socioeconomic status"
+        "Religion", "Sexual", "Socioeconomic"
     ]
 
     client = OpenAI(api_key="EMPTY", base_url="http://127.0.0.1:8000/v1")
