905ca1ae73eb3bf16254d94400f8c6f99b531863
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2025-10-04 23:13:04 +0330

    Update vllm_model_hr.py

--- src/experiments/vllm_model_hr.py
+++ src/experiments/vllm_model_hr.py
@@ -120,8 +120,8 @@ def main(args):
 
     for c in categories:
         print(c)
-        run_for_dataset(f"hr_dataset_{c}", f"hr_results_{c}", client, args.model)
-        run_for_dataset(f"current_hr_dataset_{c}", f"curr_hr_results_{c}", client, args.model)
+        run_for_dataset(f"hr_dataset_{c}", f"post_hr_results_{c}", client, args.model)
+        run_for_dataset(f"current_hr_dataset_{c}", f"curr_post_hr_results_{c}", client, args.model)
 
 
 def get_args():
