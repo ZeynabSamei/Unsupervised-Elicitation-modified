d61d94814aaa80d8d96551e5c59108da3d89835f
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2025-10-04 07:30:14 +0330

    Update vllm_model.py

--- src/experiments/vllm_model.py
+++ src/experiments/vllm_model.py
@@ -52,8 +52,8 @@ def load_data(args):
 
     
     # system_prompt=system_prompt+"\n"+system_prompt1
-    system_prompt=system_prompt1+ "Now do you think the claim is morally True (1) or False (0)?"
-    # system_prompt=system_prompt1+" Examples:"+fewshot_text+ "Now do you think the claim is morally True (1) or False (0)?"
+    # system_prompt=system_prompt1+ "Now do you think the claim is morally True (1) or False (0)?"
+    system_prompt=system_prompt1+" Examples:"+fewshot_text+ "Now do you think the claim is morally True (1) or False (0)?"
     
     # print(system_prompt)  # Just to check it's loaded
     
