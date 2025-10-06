aad4b7a0291b661608cc7eb5e0e2457ae45fb3ef
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2025-10-06 22:38:46 +0330

    Update ICM.py

--- src/experiments/ICM.py
+++ src/experiments/ICM.py
@@ -401,13 +401,14 @@ I think this claim is **** """
     elif args.testbed == "commonsense_dataset":
         with open(get_root_directory() / "data/train_commonsense_dataset.json") as f:
             train = json.load(f)
-        template = """Claim: {claim}
+        template = """statement: {statement}
+        claim: {claim}
 I think this claim is **** """
 
         for i in train:
             i['source'] = 'commonsense_dataset'
             i['consistency_key'] = 'A' if i['label'] else 'B'
-            i['prompt'] = template.format(claim=i['claim'])
+            i['prompt'] = template.format(statement=i['statement'], claim=i['claim'])
         args.GROUP_SIZE = 1
 
 
