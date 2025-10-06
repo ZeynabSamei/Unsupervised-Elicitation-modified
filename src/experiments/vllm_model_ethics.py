fd90c3024ee08700833d9bf5c1426538d615b6a8
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2025-10-06 21:21:49 +0330

    Update vllm_model_ethics.py

--- src/experiments/vllm_model_ethics.py
+++ src/experiments/vllm_model_ethics.py
@@ -160,7 +160,7 @@ def run_for_dataset(dataset_name, save_name, client, model, c):
 
 def main(args):
     # categories = ["commonsense", "deontology", "justice", "util"]
-    categories = ["deontology","justice"]
+    categories = ["util"]
     
 
     client = OpenAI(api_key="EMPTY", base_url="http://127.0.0.1:8000/v1")
